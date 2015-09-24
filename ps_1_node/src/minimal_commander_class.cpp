5aa4654b543814dccfa35018283477c4f0b4c232
lusu8892 <sxl924@case.edu>
2015-09-24 12:27:01 -0400

    update

--- ps_1_node/src/minimal_commander_class.cpp
+++ ps_1_node/src/minimal_commander_class.cpp
@@ -11,8 +11,9 @@
 class MinimalCommanderClass
 {
 public:
-    MinimalCommanderClass(ros::NodeHandle* nodehandle, double rate); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
+    MinimalCommanderClass(ros::NodeHandle* nodehandle); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
     // may choose to define public methods or public variables, if desired
+    ~MinimalCommanderClass();
     void velProfileGen(); // this is a member function which is used to generate velocity profile and then publish it to the topic "vel_cmd"
 private:
     // put private member data here;  "private" data will only be available to member functions of this class;
@@ -26,11 +27,12 @@ private:
     std_msgs::Float64 frequency_; // example member variable: better than using globals; convenient way to pass data from a subscriber to other member functions
 
     double dt_; //10ms integration time step 
-    // double sample_rate_; // compute the corresponding update frequency 
+    double sample_rate_; // compute the corresponding update frequency 
     double time_;
 
     int num_count_;
-    ros::Rate naptime_;
+    // ros::Rate naptime_;
+    ros::Rate* naptime_pointer_;
     // member methods as well:
     void initializePublishers();
     void initializeServices();
@@ -40,7 +42,7 @@ private:
     
 };
 
-MinimalCommanderClass::MinimalCommanderClass(ros::NodeHandle* nodehandle, double rate):nh_(*nodehandle), naptime_(rate)
+MinimalCommanderClass::MinimalCommanderClass(ros::NodeHandle* nodehandle):nh_(*nodehandle) //, naptime_(rate)
 { // constructor
     ROS_INFO("in class constructor of MinimalCommanderClass");
     // package up the messy work of creating subscribers; do this overhead in constructor
@@ -51,12 +53,16 @@ MinimalCommanderClass::MinimalCommanderClass(ros::NodeHandle* nodehandle, double
     amplitute_.data = 0.0;
     frequency_.data = 0.0; // example member variable: better than using globals; convenient way to pass data from a subscriber to other member functions
     dt_ = 0.01; //10ms integration time step
-    // sample_rate_ = 10.0 ;// compute the corresponding update frequency
+    sample_rate_ = 10.0 ;// compute the corresponding update frequency
     time_ = 0.0;
     num_count_ = 0;
-     
+    naptime_pointer_ = new ros::Rate (sample_rate_);
 
     // can also do tests/waits to make sure all required services, topics, etc are alive
+}
+
+MinimalCommanderClass::~MinimalCommanderClass() {
+    delete naptime_pointer_;
 }
 
 void MinimalCommanderClass::velProfileGen() {
@@ -68,7 +74,7 @@ void MinimalCommanderClass::velProfileGen() {
         ROS_INFO("velocity command = %f", vel_cmd_.data);
         ROS_INFO("I am running");
         ros::spinOnce(); //allow data update from callback; 
-        naptime_.sleep(); // wait for remainder of specified period;
+        naptime_pointer_ -> sleep(); // wait for remainder of specified period;
     }   
 }
 
@@ -110,8 +116,8 @@ int main(int argc, char **argv) {
     // when this compiled code is run, ROS will recognize it as a node called "minimal_commander" 
     ros::NodeHandle nh; // node handle 
     
-    double ros_rate = 10.0;
-    MinimalCommanderClass minimalCommanderClass(&nh,ros_rate);
+    //double ros_rate = 10.0;
+    MinimalCommanderClass minimalCommanderClass(&nh);
 
     minimalCommanderClass.velProfileGen();
     return 0; // should never get here, unless roscore dies 
