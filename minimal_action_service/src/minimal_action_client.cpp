00bfeb4d7c54cca6ac760c9fd260c2959996ca01
lusu8892 <sxl924@case.edu>
2015-09-29 22:46:21 -0400

    update source files

--- minimal_action_service/src/minimal_action_client.cpp
+++ minimal_action_service/src/minimal_action_client.cpp
@@ -18,29 +18,30 @@ using namespace std;
 class MinimalActionClientClass
 {
 public:
-    MinimalActionClientClass(ros::NodeHandle* nodehandle); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
+    MinimalActionClientClass(); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
     // may choose to define public methods or public variables, if desired
     ~MinimalActionClientClass();
     // void executeActionCB(const actionlib::SimpleActionServer<minimal_action_service/minimal_action_msgAction>::GoalConstPtr& goal);
     // prototype for action service callback function, the agrument is a pointer to a goal message
-    void actonClient();
+    int actonClientMem();
 private:
     // put private member data here;  "private" data will only be available to member functions of this class;
     // use the name of our server, which is: minimal_action (named in minimal_commander_class.cpp)
-    actionlib::SimpleActionClient<minimal_action_service::demoAction> action_client_;
+    actionlib::SimpleActionClient<minimal_action_service::minimal_action_msgAction> action_client_;
 
     // some private member variable
     // here is a "goal" object compatible with the server, as defined in example_action_server/action
-    minimal_action_service::minimal_action_msgGoal goal_; 
+    minimal_action_service::minimal_action_msgGoal goal_;
+    string answer_;
     bool server_exists_;
     bool finished_before_timeout_;
-    void doneActionCB(const const actionlib::SimpleClientGoalState& state, const minimal_action_service::minimal_action_msgResultConstPtr& result);
+    void doneActionCB(const actionlib::SimpleClientGoalState& state, const minimal_action_service::minimal_action_msgResultConstPtr& result);
 
 };
 
 // use the name of our server, which is: example_action (named in example_action_server.cpp)
 // the "true" argument says that we want our new client to run as a separate thread (a good idea)
-MinimalActionClientClass::MinimalActionClientClass() : action_client_("minimal_action", ture) 
+MinimalActionClientClass::MinimalActionClientClass() : action_client_("minimal_action", true)
 { // constructor
     ROS_INFO("in class constructor of MinimalActionClientClass");
 }
@@ -50,7 +51,7 @@ MinimalActionClientClass::~MinimalActionClientClass()
 	ROS_INFO("in class deconstructor of MinimalActionClientClass");
 }
 
-MinimalActionClientClass::actionClient()
+int MinimalActionClientClass::actonClientMem()
 {
 	// attempt to connect to the server:
     ROS_INFO("waiting for server: ");
@@ -60,32 +61,32 @@ MinimalActionClientClass::actionClient()
     // something odd in above: does not seem to wait for 5 seconds, but returns rapidly if server not running
     // bool server_exists = action_client_.waitForServer(); //wait forever
 
-    if (!server_exists) {
+    if (!server_exists_) {
         ROS_WARN("could not connect to server; halting");
         return 0; // bail out; optionally, could print a warning message and retry
     }
     ROS_INFO("connected to action server");  // if here, then we connected to the server;
 
-    while(true) {
-	    cout << endl;
-	    cout << "Do you want to change the amplitue and frequency? (Y/N)";
-	    cin >> answer;
-	    cout << endl;
-    	if (answer.compare("Y") == 0 || answer.compare("y") == 0) {
-			cout << "Please enter the value of amplitude and frequency: ";
-			cin >> goal_.amplitude_input >> goal_.frequency_input >> goal_.cycles_input;
-
+	cout << endl;
+    cout << "Do you want to change the amplitue and frequency? (Y/N)";
+    cin >> answer_;
+    cout << endl;
+    if (answer_.compare("Y") == 0 || answer_.compare("y") == 0){
+    	cout << "Please enter the value of amplitude, frequency, and cycles: ";
+		cin >> goal_.amplitude_input >> goal_.frequency_input >> goal_.cycles_input;
+    	while(true) {
 			// action_client_.sendGoal(goal); // simple example--send goal, but do not specify callbacks
-			action_client_.sendGoal(goal_,&doneActionCB); // we could also name additional callback functions here, if desired
+			action_client_.sendGoal(goal_,boost::bind(&MinimalActionClientClass::doneActionCB,this,_1,_2)); 
+			// we could also name additional callback functions here, if desired
 			// action_client_.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); //e.g., like this
 
-			bool finished_before_timeout = action_client_.waitForResult(ros::Duration(5.0));
+			finished_before_timeout_ = action_client_.waitForResult(ros::Duration(5.0));
 			// this causes the client to suspend, waiting for on the server, but with a specified time-out limit.
 			// if the server returns within the specified time limit, the goalCB functio with be triggered.
 			// this function receives the "result" message provided by the server.
 			// bool finished_before_timeout = action_client_.waitForResult(); // wait forever...
 			
-			if (!finished_before_timeout) {
+			if (!finished_before_timeout_) {
 			    ROS_WARN("giving up waiting on result for goal number");
 			    return 0;
 			}
@@ -93,12 +94,11 @@ MinimalActionClientClass::actionClient()
 			  //if here, then server returned a result to us
 			}
 		}
-        else {
-        	ROS_INFO("Quit");
-        	return 1;
-        }
-    	return 0;
 	}
+    else {
+        ROS_INFO("Quit");
+        return 1;
+    }
 }
 
 // This function will be called once when the goal completes
@@ -107,15 +107,15 @@ void MinimalActionClientClass::doneActionCB(const actionlib::SimpleClientGoalSta
         const minimal_action_service::minimal_action_msgResultConstPtr& result) 
 {
     ROS_INFO("doneActionCB: server responded with state [%s]", state.toString().c_str());
-    ROS_INFO("amplitude = %d; frequency = %d; cycles = %d, outcome = [%s]", 
-    		result->amplitde_output, result->frequency_output, result->cycles_output, result->outcome);
+    ROS_INFO("amplitude = %f; frequency = %f; cycles = %f, outcome = [%s]", 
+    		result->amplitude_output, result->frequency_output, result->cycles_output, result->outcome.c_str());
 }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "minimal_action_client_node"); // name this node 
        
     MinimalActionClientClass minimalActionClientClass;
-    minimalActionClientClass.actionClient();
+    minimalActionClientClass.actonClientMem();
 
     return 0;
 }
